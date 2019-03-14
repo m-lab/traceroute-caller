6499b4ee08679e91b8897c451401bb3bd1a8f67e
yachang <changya@gmail.com>
2019-03-14 13:29:31 -0400

    caller fix

--- caller.go
+++ caller.go
@@ -4,27 +4,29 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/m-lab/traceroute-caller/connectionwatcher"
 	"github.com/m-lab/traceroute-caller/scamper"
 )
 
-var outputPath = flag.String("outputPath", "/var/spool/scamper", "path of output")
-
+// Sample cmd:
+// go build
+// ./traceroute-caller --outputPath scamper_output
 func main() {
+	var outputPath string
+	flag.StringVar(&outputPath, "outputPath", "/var/spool/scamper", "path of output")
+	flag.Parse()
+
 	var connWatcher connectionwatcher.ConnectionWatcher
-	if len(os.Args) > 1 {
-		*outputPath = os.Args[1]
-	}
+	connWatcher.Init()
 	connWatcher.GetConnections()
-	for true {
+	for {
 		closedCollection := connWatcher.GetClosedCollection()
 		fmt.Printf("length of closed connections: %d\n", len(closedCollection))
 		for _, conn := range closedCollection {
 			log.Printf("PT start: %s %d", conn.Remote_ip, conn.Remote_port)
-			go scamper.Run(conn, *outputPath)
+			go scamper.Run(conn, outputPath)
 		}
 		time.Sleep(5 * time.Second)
 	}
