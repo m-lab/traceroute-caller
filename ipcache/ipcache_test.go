94c6a301ea7c782a7aea2d37b7d359b626cb07bd
Peter Boothe <pboothe@google.com>
2019-12-04 09:52:42 -0500

    Fix deadlock bug in ipcache (#43)
    * Fix deadlock bug in ipcache
    * Make sure autotools are installed for build

--- ipcache/ipcache_test.go
+++ ipcache/ipcache_test.go
@@ -2,7 +2,6 @@ package ipcache_test
 
 import (
 	"context"
-	"log"
 	"testing"
 	"time"
 
@@ -12,43 +11,17 @@ import (
 
 type testTracer struct {
 	calls   int
+	cctest  int
 	answers []map[connection.Connection]struct{}
 }
 
 func (tf *testTracer) Trace(conn connection.Connection, t time.Time) string {
-	log.Println("Create Trace Test")
-	return "Fake Trace test " + conn.RemoteIP
+	return "Fake trace test " + conn.RemoteIP
 }
 
 func (tf *testTracer) CreateCacheTest(conn connection.Connection, t time.Time, cachedTest string) {
-	log.Println("Create cached Test " + conn.RemoteIP)
+	tf.cctest++
 	return
-}
-
-func TestGetData(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	var tt testTracer
-	testCache := ipcache.New(ctx, &tt, 100*time.Second, time.Second)
-
-	conn1 := connection.Connection{
-		RemoteIP:   "1.1.1.2",
-		RemotePort: 5034,
-		LocalIP:    "1.1.1.3",
-		LocalPort:  58790,
-		Cookie:     "10f3d"}
-
-	testCache.Trace(conn1)
-	time.Sleep(200 * time.Millisecond)
-	tmp := testCache.GetData("1.1.1.1")
-	if tmp != "" {
-		t.Error("GetData not working correctly ")
-	}
-
-	tmp = testCache.GetData("1.1.1.2")
-	if tmp != "Fake Trace test 1.1.1.2" {
-		t.Error("GetData not working correctly ")
-	}
 }
 
 func TestTrace(t *testing.T) {
@@ -64,25 +37,29 @@ func TestTrace(t *testing.T) {
 		LocalPort:  58790,
 		Cookie:     "10f3d"}
 
-	testCache.Trace(conn1)
-	time.Sleep(200 * time.Millisecond)
-	tmp := testCache.GetData("1.1.1.2")
-	if tmp != "Fake Trace test 1.1.1.2" {
+	tmp := testCache.Trace(conn1)
+	if tmp != "Fake trace test 1.1.1.2" {
 		t.Error("cache not trace correctly ")
 	}
 
 	conn2 := connection.Connection{
 		RemoteIP:   "1.1.1.5",
 		RemotePort: 5034,
-		LocalIP:    "1.1.1.7",
+		LocalIP:    "1.1.1.3",
 		LocalPort:  58790,
 		Cookie:     "aaaa"}
 
-	testCache.Trace(conn2)
-	testCache.Trace(conn1)
-
-	time.Sleep(200 * time.Millisecond)
-
+	t2 := testCache.Trace(conn2)
+	if t2 != "Fake trace test 1.1.1.5" {
+		t.Error("cache did not trace")
+	}
+	if tt.cctest != 0 {
+		t.Errorf("Should have had zero calls to CreateCachedTest, not %d", tt.cctest)
+	}
+	testCache.Trace(conn1) // This should be cached
+	if tt.cctest != 1 {
+		t.Errorf("Should have had one call to CreateCachedTest, not %d", tt.cctest)
+	}
 	if testCache.GetCacheLength() != 2 {
 		t.Error("cache not working correctly ")
 	}
@@ -101,12 +78,12 @@ func TestRecentIPCache(t *testing.T) {
 		Cookie:     "11",
 	})
 	if tmp.GetCacheLength() != 1 {
-		t.Error("cache not working correctly")
+		t.Error("Did not put an entry into the cache")
 	}
 
 	time.Sleep(300 * time.Millisecond)
 	if tmp.GetCacheLength() != 0 {
-		t.Error("cache not working correctly")
+		t.Error("Cache GC failed to collect garbage")
 	}
 	cancel()
 	time.Sleep(200 * time.Millisecond)
