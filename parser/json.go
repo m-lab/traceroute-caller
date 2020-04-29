15c53a6a3330ee3e0670c8c6495ff8372a3b5ae5
yachang <changya@gmail.com>
2020-04-29 19:27:31 -0400

    more test coverage

--- parser/json.go
+++ parser/json.go
@@ -127,6 +127,20 @@ func GetGeoAnnotation(ann *annotator.ClientAnnotations) api.GeolocationIP {
 
 func InsertAnnotation(ann map[string]*annotator.ClientAnnotations,
 	data []byte) ([]byte, error) {
+	PTTest, err := ParseAndInsertAnnotation(ann, data)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	outputString, err := json.Marshal(PTTest)
+	if err == nil {
+		return []byte(outputString), nil
+	}
+	return []byte{}, errors.New("Cannot marshal into json")
+}
+
+func ParseAndInsertAnnotation(ann map[string]*annotator.ClientAnnotations,
+	data []byte) (schema.PTTest, error) {
 	var uuid, version string
 	var resultFromCache bool
 	var hops []schema.ScamperHop
@@ -138,201 +152,11 @@ func InsertAnnotation(ann map[string]*annotator.ClientAnnotations,
 	jsonStrings := strings.Split(string(data[:]), "\n")
 	if len(jsonStrings) != 5 {
 		log.Println("Invalid test")
-		return []byte{}, errors.New("Invalid test")
-	}
-
-	// Parse the first line for meta info.
-	err := json.Unmarshal([]byte(jsonStrings[0]), &meta)
-	log.Println(meta)
-	if err != nil {
-		log.Println(err)
-		return []byte{}, errors.New("Invalid meta")
-	}
-	if meta.UUID == "" {
-		return []byte{}, errors.New("empty UUID")
-	}
-	uuid = meta.UUID
-	version = meta.TracerouteCallerVersion
-	resultFromCache = meta.CachedResult
-
-	err = json.Unmarshal([]byte(jsonStrings[1]), &cycleStart)
-	if err != nil {
-		return []byte{}, errors.New("Invalid cycle-start")
-	}
-
-	// Parse the line in struct
-	err = json.Unmarshal([]byte(jsonStrings[2]), &tracelb)
-	if err != nil {
-		// Some early stage scamper output has JSON grammar errors that can be fixed by
-		// extra reprocessing using jsonnett
-		// TODO: this is a hack. We should see if this can be simplified.
-		vm := jsonnet.MakeVM()
-		output, err := vm.EvaluateSnippet("file", jsonStrings[2])
-		err = json.Unmarshal([]byte(output), &tracelb)
-		if err != nil {
-			// fail and return here.
-			return []byte{}, errors.New("Invalid tracelb")
-		}
-	}
-	for i, _ := range tracelb.Nodes {
-		oneNode := &tracelb.Nodes[i]
-		var links []schema.HopLink
-		if len(oneNode.Links) == 0 {
-			hops = append(hops, schema.ScamperHop{
-				Source: schema.HopIP{IP: oneNode.Addr, Hostname: oneNode.Name},
-				Linkc:  oneNode.Linkc,
-			})
-			continue
-		}
-		if len(oneNode.Links) != 1 {
-			continue
-		}
-		// Links is an array containing a single array of HopProbes.
-		for _, oneLink := range oneNode.Links[0] {
-			var probes []schema.HopProbe
-			var ttl int64
-			for _, oneProbe := range oneLink.Probes {
-				var rtt []float64
-				for _, oneReply := range oneProbe.Replies {
-					rtt = append(rtt, oneReply.Rtt)
-				}
-				probes = append(probes, schema.HopProbe{Flowid: int64(oneProbe.Flowid), Rtt: rtt})
-				ttl = int64(oneProbe.Ttl)
-			}
-			links = append(links, schema.HopLink{HopDstIP: oneLink.Addr, TTL: ttl, Probes: probes})
-		}
-		// Extract Hop Geolocation for hop IP
-		if ann[oneNode.Addr] != nil {
-			hopAnn := ann[oneNode.Addr]
-			hops = append(hops, schema.ScamperHop{
-				Source: schema.HopIP{IP: oneNode.Addr,
-					City:        hopAnn.Geo.City,
-					CountryCode: hopAnn.Geo.CountryCode,
-					ASN:         hopAnn.Network.ASNumber,
-					Hostname:    oneNode.Name},
-				Linkc: oneNode.Linkc,
-				Links: links,
-			})
-		}
-		hops = append(hops, schema.ScamperHop{
-			Source: schema.HopIP{IP: oneNode.Addr, Hostname: oneNode.Name},
-			Linkc:  oneNode.Linkc,
-			Links:  links,
-		})
-
-	}
-
-	err = json.Unmarshal([]byte(jsonStrings[3]), &cycleStop)
-	if err != nil {
-		return []byte{}, errors.New("Invalid cycle-stop")
-	}
-
-	var source schema.ServerInfo
-	if ann[tracelb.Src] == nil {
-		source = schema.ServerInfo{
-			IP:      tracelb.Src,
-			Geo:     nil,
-			Network: nil,
-		}
-	} else {
-		srcGeo := GetGeoAnnotation(ann[tracelb.Src])
-		srcNetwork := api.ASData{
-			Systems: []api.System{api.System{ASNs: []uint32{ann[tracelb.Src].Network.ASNumber}}},
-		}
-		source = schema.ServerInfo{
-			IP:      tracelb.Src,
-			Geo:     &srcGeo,
-			Network: &srcNetwork,
-		}
-	}
-
-	var dest schema.ClientInfo
-	if ann[tracelb.Dst] == nil {
-		dest = schema.ClientInfo{
-			IP:      tracelb.Dst,
-			Geo:     nil,
-			Network: nil,
-		}
-	} else {
-		destGeo := GetGeoAnnotation(ann[tracelb.Dst])
-		destNetwork := api.ASData{
-			Systems: []api.System{api.System{ASNs: []uint32{ann[tracelb.Dst].Network.ASNumber}}},
-		}
-		dest = schema.ClientInfo{
-			IP:      tracelb.Dst,
-			Geo:     &destGeo,
-			Network: &destNetwork,
-		}
-	}
-	output := schema.PTTest{
-		UUID:           uuid,
-		StartTime:      int64(cycleStart.Start_time),
-		StopTime:       int64(cycleStop.Stop_time),
-		ScamperVersion: tracelb.Version,
-		Source:         source,
-		Destination:    dest,
-		ProbeSize:      int64(tracelb.Probe_size),
-		ProbeC:         int64(tracelb.Probec),
-		Hop:            hops,
-		ExpVersion:     version,
-		CachedResult:   resultFromCache,
-	}
-	outputString, err := json.Marshal(output)
-	if err == nil {
-		return []byte(outputString), nil
-	}
-	return []byte{}, errors.New("Cannot marshal into json")
-}
-
-func ExtractIP(rawContent []byte) []string {
-	var IPList []string
-	var tracelb TracelbLine
-
-	jsonStrings := strings.Split(string(rawContent[:]), "\n")
-
-	// Parse the line in struct
-	err := json.Unmarshal([]byte(jsonStrings[2]), &tracelb)
-	if err != nil {
-		return IPList
-	}
-
-	log.Println(tracelb.Src)
-	IPList = append(IPList, tracelb.Src)
-	IPList = append(IPList, tracelb.Dst)
-
-	for i, _ := range tracelb.Nodes {
-		oneNode := &tracelb.Nodes[i]
-		IPList = append(IPList, oneNode.Addr)
-	}
-	return IPList
-}
-
-// ParseJSON the raw jsonl test file into schema.PTTest.
-func ParseJSON(testName string, rawContent []byte, taskFilename string) (schema.PTTest, error) {
-	// Get the logtime
-	logTime, err := GetLogtime(PTFileName{Name: filepath.Base(testName)})
-	if err != nil {
-		return schema.PTTest{}, err
-	}
-
-	// Split the JSON file and parse it line by line.
-	var uuid, version string
-	var resultFromCache bool
-	var hops []schema.ScamperHop
-	var meta Metadata
-	var cycleStart CyclestartLine
-	var tracelb TracelbLine
-	var cycleStop CyclestopLine
-
-	jsonStrings := strings.Split(string(rawContent[:]), "\n")
-
-	if len(jsonStrings) != 5 {
-		log.Println("Invalid test", taskFilename, "  ", testName)
 		return schema.PTTest{}, errors.New("Invalid test")
 	}
 
 	// Parse the first line for meta info.
-	err = json.Unmarshal([]byte(jsonStrings[0]), &meta)
+	err := json.Unmarshal([]byte(jsonStrings[0]), &meta)
 	log.Println(meta)
 	if err != nil {
 		log.Println(err)
@@ -391,6 +215,19 @@ func ParseJSON(testName string, rawContent []byte, taskFilename string) (schema.
 			}
 			links = append(links, schema.HopLink{HopDstIP: oneLink.Addr, TTL: ttl, Probes: probes})
 		}
+		// Extract Hop Geolocation for hop IP
+		if ann[oneNode.Addr] != nil {
+			hopAnn := ann[oneNode.Addr]
+			hops = append(hops, schema.ScamperHop{
+				Source: schema.HopIP{IP: oneNode.Addr,
+					City:        hopAnn.Geo.City,
+					CountryCode: hopAnn.Geo.CountryCode,
+					ASN:         hopAnn.Network.ASNumber,
+					Hostname:    oneNode.Name},
+				Linkc: oneNode.Linkc,
+				Links: links,
+			})
+		}
 		hops = append(hops, schema.ScamperHop{
 			Source: schema.HopIP{IP: oneNode.Addr, Hostname: oneNode.Name},
 			Linkc:  oneNode.Linkc,
@@ -404,18 +241,96 @@ func ParseJSON(testName string, rawContent []byte, taskFilename string) (schema.
 		return schema.PTTest{}, errors.New("Invalid cycle-stop")
 	}
 
-	return schema.PTTest{
+	var source schema.ServerInfo
+	if ann[tracelb.Src] == nil {
+		source = schema.ServerInfo{
+			IP:      tracelb.Src,
+			Geo:     nil,
+			Network: nil,
+		}
+	} else {
+		srcGeo := GetGeoAnnotation(ann[tracelb.Src])
+		srcNetwork := api.ASData{
+			Systems: []api.System{api.System{ASNs: []uint32{ann[tracelb.Src].Network.ASNumber}}},
+		}
+		source = schema.ServerInfo{
+			IP:      tracelb.Src,
+			Geo:     &srcGeo,
+			Network: &srcNetwork,
+		}
+	}
+
+	var dest schema.ClientInfo
+	if ann[tracelb.Dst] == nil {
+		dest = schema.ClientInfo{
+			IP:      tracelb.Dst,
+			Geo:     nil,
+			Network: nil,
+		}
+	} else {
+		destGeo := GetGeoAnnotation(ann[tracelb.Dst])
+		destNetwork := api.ASData{
+			Systems: []api.System{api.System{ASNs: []uint32{ann[tracelb.Dst].Network.ASNumber}}},
+		}
+		dest = schema.ClientInfo{
+			IP:      tracelb.Dst,
+			Geo:     &destGeo,
+			Network: &destNetwork,
+		}
+	}
+	output := schema.PTTest{
 		UUID:           uuid,
-		TestTime:       logTime,
 		StartTime:      int64(cycleStart.Start_time),
 		StopTime:       int64(cycleStop.Stop_time),
 		ScamperVersion: tracelb.Version,
-		Source:         schema.ServerInfo{IP: tracelb.Src},
-		Destination:    schema.ClientInfo{IP: tracelb.Dst},
+		Source:         source,
+		Destination:    dest,
 		ProbeSize:      int64(tracelb.Probe_size),
 		ProbeC:         int64(tracelb.Probec),
 		Hop:            hops,
 		ExpVersion:     version,
 		CachedResult:   resultFromCache,
-	}, nil
+	}
+	return output, nil
+}
+
+func ExtractIP(rawContent []byte) []string {
+	var IPList []string
+	var tracelb TracelbLine
+
+	jsonStrings := strings.Split(string(rawContent[:]), "\n")
+
+	// Parse the line in struct
+	err := json.Unmarshal([]byte(jsonStrings[2]), &tracelb)
+	if err != nil {
+		return IPList
+	}
+
+	log.Println(tracelb.Src)
+	IPList = append(IPList, tracelb.Src)
+	IPList = append(IPList, tracelb.Dst)
+
+	for i, _ := range tracelb.Nodes {
+		oneNode := &tracelb.Nodes[i]
+		IPList = append(IPList, oneNode.Addr)
+	}
+	return IPList
+}
+
+// ParseJSON the raw jsonl test file into schema.PTTest.
+func ParseJSON(testName string, rawContent []byte) (schema.PTTest, error) {
+	// Get the logtime
+	logTime, err := GetLogtime(PTFileName{Name: filepath.Base(testName)})
+	if err != nil {
+		return schema.PTTest{}, err
+	}
+
+	emptyAnn := make(map[string]*annotator.ClientAnnotations)
+	PTTest, err := ParseAndInsertAnnotation(emptyAnn, rawContent)
+
+	if err != nil {
+		return schema.PTTest{}, err
+	}
+	PTTest.TestTime = logTime
+	return PTTest, nil
 }
