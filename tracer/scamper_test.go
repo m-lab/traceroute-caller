9fe92ece60070c6a27932a3d0bff7daf455fac0c
Saied Kazemi <saied@google.com>
2021-06-23 18:29:02 -0700

    Change the exepcted error message in scamper_test.go

--- tracer/scamper_test.go
+++ tracer/scamper_test.go
@@ -240,7 +240,7 @@ func TestTraceTimeout(t *testing.T) {
 	faketime := time.Date(2019, time.April, 1, 3, 45, 51, 0, time.UTC)
 	prometheusx.GitShortCommit = "Fake Version"
 	data, err := d.Trace(c, faketime)
-	if err.Error() != "timeout" {
+	if err.Error() != "context deadline exceeded" {
 		t.Error("Should return TimeOut err, not ", err)
 	}
 	if data != "" {
